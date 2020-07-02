475b1fc62417cf78428a246f90904844d8711c9f
Maycon-queiroz <fmaycon@gmail.com>
2020-07-02 16:49:30 -0300

    depoy heroku

---
+++ .profile.d/file-permissions.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Sets WordPress file permissions for a secure environment
+
+# - create folders
+mkdir -p $HOME/web/app/uploads
+mkdir -p $HOME/web/app/themes
+
+# - deny write on everything
+chmod -R a-w $HOME/web
+
+# - allow write to web/app/uploads
+chmod -R u+w $HOME/web/app/uploads
+chmod -R u+w $HOME/web/app/themes
